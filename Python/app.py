62f5b78b8723178c72e8795be601cda3ead665ee
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2023-05-19 12:40:43 +0000

    py

--- Python/app.py
+++ Python/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import send_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/books")
-
-def books():
-    return send_file('books.json')
+@app.route("/noncapisconulla")
+def gjs():
+    return send_file('prova.json')
 
 if __name__ == "__main__":
     app.run()
